6a62a54f49cc20b31e710c729ef11c33e151984c
Artem Petrov <artempetrovjob@mail.ru>
2025-05-30 12:35:32 +0300

    style: форматирование

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      '@types': path.resolve(__dirname, 'src/types'),
+      "@types": path.resolve(__dirname, "src/types"),
     },
   },
 });
